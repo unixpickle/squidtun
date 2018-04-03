9b9dceef2fce293bbbd8254a0b6ec5c8f0a2aa59
Alex Nichol <unixpickle@gmail.com>
2018-04-03 00:22:08 -0400

    drafted stream for reading chunks
    This will be used to read chunks from the local socket and forward them to the remote end. We will need a similar thing that can take chunks from the remote end and push them to the local socket. For that case, I'll probably just make a Future that writes a single chunk (buffering happens on the OS layer, anyway).

--- src/client/future_util.rs
+++ src/client/future_util.rs
@@ -1,7 +1,41 @@
-use futures::{Future, IntoFuture, Stream};
-use tokio_io::{AsyncRead, AsyncWrite};
+use std::io;
+use std::iter::repeat;
+
+use futures::{Async, Future, IntoFuture, Poll, Stream};
+use tokio_io::AsyncRead;
 
 /// Get a Future for when the stream completes.
 pub fn wait_for_end<S: Stream + 'static>(s: S) -> Box<Future<Item = (), Error = S::Error>> {
     Box::new(s.for_each(|_| Ok(()).into_future()))
+}
+
+pub struct ReadStream<T: AsyncRead> {
+    reader: T,
+    buf_size: usize
+}
+
+impl<T: AsyncRead> ReadStream<T> {
+    pub fn new(reader: T, buf_size: usize) -> ReadStream<T> {
+        ReadStream{reader: reader, buf_size: buf_size}
+    }
+}
+
+impl<T: AsyncRead> Stream for ReadStream<T> {
+    type Item = Vec<u8>;
+    type Error = io::Error;
+
+    fn poll(&mut self) -> Poll<Option<Self::Item>, Self::Error> {
+        let mut res: Vec<u8> = repeat(0).take(self.buf_size).collect();
+        match self.reader.poll_read(&mut res) {
+            Ok(Async::Ready(size)) => {
+                if size == 0 {
+                    Ok(Async::Ready(None))
+                } else {
+                    Ok(Async::Ready(Some(res[..size].to_vec())))
+                }
+            },
+            Ok(Async::NotReady) => Ok(Async::NotReady),
+            Err(e) => Err(e)
+        }
+    }
 }
